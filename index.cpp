b088b1ab350bdb639845a1fae74dd45cf5a5bbaa
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-02-03 23:11:34 +0530

    update

--- index.cpp
+++ index.cpp
@@ -1,13 +1,14 @@
-# include<stdio.h>
+# include<iostream>
+using namespace std;
 
 int main(){
-    int a;
-    a=5;
+    cout<<"hello World";
+    int a=5;
     float b=12.78;
     char c='r';
-    // printf("hello \n");
-    // printf("hello %d \n",a);
-    // printf("hello %f \n",b);
-    // printf("hello %c \n",c);
+
+
+
+
     return 0;
 }
