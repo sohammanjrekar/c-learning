03c21e93fa24f9f3d6ab8b9b08e887c1f96b9929
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-02-03 20:42:56 +0530

    update

---
+++ index.cpp
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main(){
+    int a;
+    a=5;
+    float b=12.78;
+    char c='r';
+    // printf("hello \n");
+    // printf("hello %d \n",a);
+    // printf("hello %f \n",b);
+    // printf("hello %c \n",c);
+    return 0;
+}
